11d6ca71708aeb478c102dfe87010662dbc6bf08
wangwenjun <532500648@qq.com>
2021-02-13 16:02:49 +0800

    add the application.properties under src/test/resources used replace mysql

--- src/test/java/com/wangwenjun/simple/application/controller/EmployeeControllerTest.java
+++ src/test/java/com/wangwenjun/simple/application/controller/EmployeeControllerTest.java
@@ -1,17 +1,15 @@
-package com.wangwenjun.simple.application;
+package com.wangwenjun.simple.application.controller;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class SimpleApplicationTests {
+public class EmployeeControllerTest {
 
-  @Test
-  public void contextLoads() {
-  }
 }
